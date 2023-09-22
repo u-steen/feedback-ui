3f03e974038ddbc430cc7767df23e73b2dbed7e5
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-22 10:36:38 +0300

    feedback stats bar

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,14 @@
+function FeedbackStats({feedback}){
+    let average = feedback.reduce((acc, item) => {
+        return acc += item.rating;
+    }, 0);
+    average = average / feedback.length.toFixed(1);
+    return (
+        <div className='feedback-stats'>
+            <h4>{`${feedback.length} ${feedback.length === 1 ? "Review" : "Reviews"}`}</h4>
+            <h4>Average: {isNaN(average) ? "0" : average}</h4>
+        </div>
+    )
+}
+
+export default FeedbackStats;
