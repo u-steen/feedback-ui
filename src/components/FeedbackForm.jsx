21dcb2b7ad262dd8315a625cd19b063830abfd41
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-22 19:12:52 +0300

    text field vaildation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,27 @@
 import {useState} from "react";
 import Card from "./shared/Card"
+import Button from "./shared/Button";
 
 function FeedbackForm(){
     const [text, setText] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
 
     function handleTextChange(e){
+        // BUG: 1 character late - setState is async - useEffect is the replacement
         setText(e.target.value);
+        // console.log(`Acum text este: ${text}`)
+
+        if(text === '') {
+            setMessage(null)
+            setBtnDisabled(true)
+        } else if(text.trim().length < 10) {
+            setMessage("The review must be at least 10 characters long")
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
     }
 
     return (
@@ -14,9 +30,10 @@ function FeedbackForm(){
                 <h2>Give us feedback</h2>
                 <div className="input-group">
                     <input type="text" placeholder="Write a review" onChange={handleTextChange} value={text}/>
-                    <button>Send</button>
+                    <Button type="submit" version="secondary" isDisabled={btnDisabled}>Send</Button>
                 </div>
             </form>
+            {message && <div>{message}</div>}
         </Card>
     )
 }
---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+function Button({children, version, type, isDisabled}){
+    return (
+        <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+            {children}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    version: "primary",
+    isDisabled: false,
+    type: "button"
+}
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    version: PropTypes.string,
+    isDisabled: PropTypes.bool,
+    type: PropTypes.string
+}
+
+export default Button
