477eca04e0af2722d78afc2224e252a97d971e2d
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-23 11:12:23 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {useState} from "react";
 import {v4 as uuidv4} from "uuid"
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from "./pages/AboutPage";
 
 import feedbackData from "./data/feedbackData";
 
@@ -23,14 +25,22 @@ function App(){
         setFeedback([newFeedback, ...feedback])
     }
 
-    return (<>
-        <Header/>
-        <FeedbackForm addFeedback={addFeedback}/>
-        <FeedbackStats feedback={feedback}/>
-        <div className="container">
-            <FeedbackList feedback={feedback} handleDeleteItem={deleteFeedbackItem}/>
-        </div>
-    </>)
+    return (
+        <Router>
+            <Routes>
+                <Route exact path={"/"} element={
+                    <>
+                        <Header/>
+                        <FeedbackForm addFeedback={addFeedback}/>
+                        <FeedbackStats feedback={feedback}/>
+                        <div className="container">
+                            <FeedbackList feedback={feedback} handleDeleteItem={deleteFeedbackItem}/>
+                        </div>
+                    </>
+                }/>
+                <Route path={"/about"} element={<AboutPage/>}/>
+            </Routes>
+        </Router>)
 }
 
 export default App;
