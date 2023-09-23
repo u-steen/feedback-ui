6b163f7b5eb71c306ff4f070552b46037d465c29
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-23 16:40:09 +0300

    link to main page added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,12 @@
 import Card from "../components/shared/Card"
+import {Link} from "react-router-dom"
 
 function AboutPage(){
     return (
         <Card>
             <h1>About</h1>
             <p>This is an UI ment to accept and display feedback from users</p>
+            <Link to={"/"}>Back to Homepage</Link>
         </Card>
     )
 
