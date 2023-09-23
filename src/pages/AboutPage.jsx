87555337257bce984758a86748b71a3803115f41
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-23 11:12:08 +0300

    about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/shared/Card"
+
+function AboutPage(){
+    return (
+        <Card>
+            <h1>About</h1>
+            <p>This is an UI ment to accept and display feedback from users</p>
+        </Card>
+    )
+
+}
+
+export default AboutPage;
