98f6635b87c72700ec6be7cabd5eb779ede3ee6d
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-24 19:33:54 +0300

    spinner component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,15 +2,16 @@ import {useContext} from "react";
 
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
-function FeedbackList({handleDeleteItem}){
-    const {feedback} = useContext(FeedbackContext)
-    if(!feedback || feedback.length === 0) {
+function FeedbackList(){
+    const {feedback, isLoading} = useContext(FeedbackContext)
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return (<div>No feedback yet</div>);
     }
     return (
         <div className="feedback-list">
-            {feedback.map((item, id) => <FeedbackItem key={id} item={item}/>)}
+            {isLoading ? <Spinner/> : feedback.map((item, id) => <FeedbackItem key={id} item={item}/>)}
         </div>
     )
 }
